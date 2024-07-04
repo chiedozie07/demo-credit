4e062b254a59807cbcc11d03aaebd0b991e14544
skillztec <skillztech19@gmail.com>
2024-07-04 23:44:47 +0100

    structure refactor

---
+++ src/migrations/20240704205319_users.ts
@@ -0,0 +1,28 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    // Create the new users table with correct schema
+    await knex.schema.createTable('users', (table) => {
+      table.increments('id').unique().primary();
+      table.string('first_name').notNullable(); 
+      table.string('last_name').notNullable();
+      table.string('phone').unique().notNullable(); 
+      table.string('account_no').unique().nullable(); 
+      table.string('next_of_kind').notNullable(); 
+      table.date('dob').notNullable(); 
+      table.string('email', 255).unique().notNullable(); 
+      table.string('password').notNullable(); 
+      table.decimal('balance').defaultTo(0); 
+      table.timestamp('created_at').defaultTo(knex.fn.now()); 
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+      table.string('token', 255).nullable(); 
+      table.timestamps(true, true); 
+    });
+  };
+  
+  export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists('users');
+  };
+
+
+  

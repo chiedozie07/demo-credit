a2819ee2b81d89297c2221940c38fb53e25ad981
skillztec <skillztech19@gmail.com>
2024-07-04 16:12:19 +0100

    connect with private config

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,6 +1,6 @@
 import { Knex } from 'knex';
 import dotenv from 'dotenv';
-// import { getEnvNumber } from '../helpers/utils';
+import { getEnvNumber } from '../helpers/utils';
 import mysql from 'mysql2';
 import path from 'path';
 
@@ -19,13 +19,39 @@ interface IKnexConfigProps {
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 const mysqlPrivateConnection = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.MYSQL_PRIVATE_HOST}:${process.env.MYSQL_PRIVATE_PORT}/${process.env.MYSQL_DATABASE}`;
 
+const connectDB = mysql.createConnection(urlDB);
+
 const knexConfig: IKnexConfigProps = {
   production: {
     client: 'mysql',
-    connection: urlDB || mysqlPrivateConnection,
+    connection: mysqlPrivateConnection,
     migrations: {
       directory: path.resolve(__dirname, './src/db/migrations')
     },
+  },
+  development: {
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, './src/db/migrations')
+    }
+  },
+  test: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, './src/db/migrations')
+    }
   }
 };
 

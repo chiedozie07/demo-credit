f62ca5b545532b639f3f63c8303eb9c507a0b852
skillztec <skillztech19@gmail.com>
2024-07-04 15:51:38 +0100

    fix

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -1,6 +1,6 @@
 import Knex from 'knex';
-
 import knexConfig from './knexfile';
+
 const environment = (process.env.NODE_ENV || 'development') as keyof typeof knexConfig;
 const config = knexConfig[environment];
 const knex = Knex(config);
--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -1,6 +1,6 @@
 import { Knex } from 'knex';
 import dotenv from 'dotenv';
-import { getEnvNumber } from '../helpers/utils';
+// import { getEnvNumber } from '../helpers/utils';
 // import mysql from 'mysql2';
 import path from 'path';
 
@@ -16,47 +16,16 @@ interface IKnexConfigProps {
 };
 
 // Define the database URL configuration
-const urlDB = process.env.MYSQL_URL ? process.env.MYSQL_URL : `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST || '127.0.0.1'}:${process.env.MYSQLPORT || 3306}/${process.env.MYSQLDATABASE}`;
-
-// const connection = mysql.createConnection(urlDB);
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const mysqlPrivateConnection = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.MYSQL_PRIVATE_HOST}:${process.env.MYSQL_PRIVATE_PORT}/${process.env.MYSQL_DATABASE}`;
 
 const knexConfig: IKnexConfigProps = {
   production: {
     client: 'mysql',
-    connection: urlDB || {
-      host: process.env.MYSQLHOST,
-      user: process.env.MYSQLUSER,
-      password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE,
-      port: parseInt(process.env.MYSQLPORT),
-    },
+    connection: urlDB || mysqlPrivateConnection,
     migrations: {
       directory: path.resolve(__dirname, './src/db/migrations')
     },
-  },
-  development: {
-    client: "mysql",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    migrations: {
-      directory: path.resolve(__dirname, './src/db/migrations')
-    }
-  },
-  test: {
-    client: 'mysql',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    migrations: {
-      directory: path.resolve(__dirname, './src/db/migrations')
-    }
   }
 };
 

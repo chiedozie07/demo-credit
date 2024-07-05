6e20b7f58ea20994163b0cea2e48f13a4dec6d92
skillztec <skillztech19@gmail.com>
2024-07-05 17:25:23 +0100

    added connection port property

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -18,8 +18,8 @@ const knexConfig: IKnexConfigProps = {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-        // port: Number(process.env.DB_PORT)
+        database: process.env.DB_NAME,
+        port: Number(process.env.DB_PORT)
     },
     migrations: {
       directory: path.resolve(__dirname, './src/db/migrations')
